5c477c592f316ef54e56488ff868b5499fc4b43d
lh20020727 <lh20020727@sjtu.edu.cn>
2024-01-19 15:11:00 -0800

    backdrop-filter

--- 09-Omnifood-Optimizations/script.js
+++ 09-Omnifood-Optimizations/script.js
@@ -71,7 +71,7 @@ function checkFlexGap() {
 
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
-checkFlexGap();
+// checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 

9c1281a8ef1ed6b594e2be8275268dbcdc6495ac
lh20020727 <lh20020727@sjtu.edu.cn>
2024-01-19 13:34:43 -0800

    Scroll smoothly in Safari

--- 09-Omnifood-Optimizations/script.js
+++ 09-Omnifood-Optimizations/script.js
@@ -23,8 +23,11 @@ const allLinks = document.querySelectorAll("a:link");
 allLinks.forEach((link) =>
   link.addEventListener("click", (e) => {
     e.preventDefault();
-    if (link.getAttribute("href") === "#") {
+    const href = link.getAttribute("href");
+    if (href === "#") {
       window.scrollTo({ top: 0, behavior: "smooth" });
+    } else if (href !== "#" && href.startsWith("#")) {
+      document.querySelector(href).scrollIntoView({ behavior: "smooth" });
     }
   }),
 );
